4df4ef6ba1f3c0877dc22e418a00e798593dbff8
Timothy Perrett <timothy@getintheloop.eu>
2011-08-16 23:14:47 +0100

    moved login helper inot different file

---
+++ src/main/scala/shiro/forms.scala
@@ -0,0 +1,15 @@
+package shiro
+
+import net.liftweb._, util.Helpers._, http.{DispatchSnippet,SHtml}
+import org.apache.shiro.authc.UsernamePasswordToken
+
+trait DefaultUsernamePasswordLogin extends DispatchSnippet with SubjectLifeCycle {
+  def render = {
+    var username = ""
+    var password = ""
+    "type=text" #> SHtml.text(username, username = _) &
+    "type=password" #> SHtml.password(password, password = _) &
+    "type=submit" #> SHtml.submit("Login", () => 
+      login(new UsernamePasswordToken(username, password)))
+  }
+}
