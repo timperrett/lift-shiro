6ebaac95c5d1a12d71cf80b23864ac080651888e
Timothy Perrett <timothy@getintheloop.eu>
2011-09-19 10:19:42 +0100

    Published 0.0.3

--- project/build.scala
+++ project/build.scala
@@ -1,32 +1,44 @@
 import sbt._, Keys._
+import posterous.Publish._
 
 object BuildSettings {
   val buildOrganization = "eu.getintheloop"
-  val buildVersion      = "0.0.2-SNAPSHOT"
+  val buildVersion      = "0.0.3"
   val buildScalaVersion = "2.9.0-1"
 
   val buildSettings = Defaults.defaultSettings ++ Seq (
     organization := buildOrganization,
     version      := buildVersion,
     scalaVersion := buildScalaVersion,
+    scalaVersion := "2.9.1",
     scalacOptions += "-deprecation",
-    crossScalaVersions := Seq("2.8.1", "2.9.0", "2.9.0-1"),
+    crossScalaVersions := Seq("2.8.1", "2.9.0", "2.9.0-1", "2.9.1"),
     resolvers ++= Seq(
-      Resolver.file(".m2", file(Path.userHome+"/.m2/repository")),
-      "Shiro Snapshots" at "https://repository.apache.org/content/repositories/snapshots/"
-    )
-  )
+      ScalaToolsReleases,
+      "Shiro Snapshots" at "https://repository.apache.org/content/repositories/snapshots/",
+      ".m2" at Path.userHome+"/.m2/repository"
+    ),
+    publishTo := Some("Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"),
+    credentials += Credentials(Path.userHome / ".ivy2" / ".credentials")
+  ) ++ posterousSettings
 }
 
 object SudsBuild extends Build {
-  lazy val root = Project("root", file("."),
-    settings = BuildSettings.buildSettings
+  lazy val root = Project("lift-shiro-root", file("."),
+    settings = BuildSettings.buildSettings ++ Seq(
+      name in Posterous := "Lift Shiro",
+      version in Posterous := "0.0.3"
+    )
   ) aggregate(library, example)
   
   lazy val library: Project = Project("lift-shiro", file("library"), 
     settings = BuildSettings.buildSettings ++ (
       libraryDependencies ++= Seq(
-        "net.liftweb" %% "lift-webkit" % "2.4-M2",
+        "joda-time" % "joda-time" % "1.6.2" % "compile",
+        "net.liftweb" %% "lift-common" % "2.4-M4" % "compile",
+        "net.liftweb" %% "lift-util" % "2.4-M4" % "compile",
+        "net.liftweb" %% "lift-json" % "2.4-M4" % "compile",
+        "net.liftweb" %% "lift-webkit" % "2.4-M4" % "compile",
         "org.apache.shiro" % "shiro-core" % "1.2.0-SNAPSHOT",
         "org.apache.shiro" % "shiro-web" % "1.2.0-SNAPSHOT",
         "commons-beanutils" % "commons-beanutils" % "20030211.134440"
--- project/plugins/build.sbt
+++ project/plugins/build.sbt
@@ -2,3 +2,6 @@ resolvers += "Web plugin repo" at "http://siasia.github.com/maven2"
 
 libraryDependencies <+= sbtVersion(v => "com.github.siasia" %% "xsbt-web-plugin" % ("0.1.1-"+v))
 
+libraryDependencies <+= (sbtVersion) { sv =>
+  "net.databinder" %% "posterous-sbt" % ("0.3.0_sbt" + sv)
+}
