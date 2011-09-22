95ccd4ad2060a1227fb161b8f80705a4fa733113
lunserv <lunserv@gmail.com>
2011-09-22 17:09:17 +0700

    Refactoring hasAnyRoles method

--- library/src/main/scala/shiro/utils.scala
+++ library/src/main/scala/shiro/utils.scala
@@ -36,10 +36,8 @@ private[shiro] trait Utils {
   def lacksPermission(permission: String) = 
     !hasPermission(permission)
   
-  def hasAnyRoles(roles: Seq[String]) = test { subject =>
-    roles.map(r => subject.hasRole(r.trim)
-      ).contains(true)
-  }
+  def hasAnyRoles(roles: Seq[String]) = 
+    roles exists (r => hasRole(r.trim))
 }
 
 import net.liftweb.common.{Box,Failure,Full}
