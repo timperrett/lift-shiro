6466023a64865b313825cf3e0aaaa44a679fa612
Timothy Perrett <timothy@getintheloop.eu>
2011-12-19 11:02:03 +0000

    Upgraded to SBT 0.11.2 for build and Lift 2.4-M5 as a dependency

--- project/build.scala
+++ project/build.scala
@@ -14,8 +14,8 @@ object BuildSettings {
     crossScalaVersions := Seq("2.8.1", "2.9.0", "2.9.0-1", "2.9.1"),
     resolvers ++= Seq(
       ScalaToolsReleases,
-      "Shiro Snapshots" at "https://repository.apache.org/content/repositories/snapshots/",
-      ".m2" at "file://"+Path.userHome+"/.m2/repository"
+      "Shiro Snapshots" at "https://repository.apache.org/content/repositories/snapshots/"//,
+      // ".m2" at "file://"+Path.userHome+"/.m2/repository"
     ),
     publishTo := Some("Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"),
     credentials += Credentials(Path.userHome / ".ivy2" / ".credentials")
@@ -30,7 +30,7 @@ object LiftShiroBuild extends Build {
   lazy val library: Project = Project("lift-shiro", file("library"), 
     settings = BuildSettings.buildSettings ++ (
       libraryDependencies ++= Seq(
-        "net.liftweb" %% "lift-webkit" % "2.4-M4" % "compile",
+        "net.liftweb" %% "lift-webkit" % "2.4-M5" % "compile",
         "org.apache.shiro" % "shiro-core" % "1.2.0-SNAPSHOT",
         "org.apache.shiro" % "shiro-web" % "1.2.0-SNAPSHOT",
         "commons-beanutils" % "commons-beanutils" % "20030211.134440"
@@ -41,9 +41,9 @@ object LiftShiroBuild extends Build {
   lazy val example = Project("lift-shiro-example", file("example"),
     settings = BuildSettings.buildSettings ++ (
       libraryDependencies ++= Seq(
-        "org.eclipse.jetty" % "jetty-webapp" % "7.3.0.v20110203" % "jetty",
+        "org.eclipse.jetty" % "jetty-webapp" % "7.3.0.v20110203" % "container",
         "ch.qos.logback" % "logback-classic" % "0.9.26"
       )
-    )
-  ) settings(com.github.siasia.WebPlugin.webSettings :_*) dependsOn(library)
+    ) ++ com.github.siasia.WebPlugin.webSettings
+  ) dependsOn(library)
 }
--- project/plugins/build.sbt
+++
@@ -1,5 +0,0 @@
-resolvers += "web-plugin.repo" at "http://siasia.github.com/maven2"    
-
-addSbtPlugin("com.github.siasia" %% "xsbt-web-plugin" % "0.1.2")
-
-addSbtPlugin("net.databinder" % "posterous-sbt" % "0.3.2")
