118451fa2c24d1321e8fc26664e8fbd701dec0b0
Johan Prinsloo <johan@gridx.com>
2012-03-29 09:49:06 -0700

    Focus on the user name in the login page

--- library/src/main/scala/shiro/snippet/forms.scala
+++ library/src/main/scala/shiro/snippet/forms.scala
@@ -1,13 +1,13 @@
 package shiro.snippet
 
-import net.liftweb._, util.Helpers._, http.{DispatchSnippet,SHtml}
+import net.liftweb._, util.Helpers._, http.{DispatchSnippet,SHtml}, http.js.JsCmds._
 import org.apache.shiro.authc.UsernamePasswordToken
 
 trait DefaultUsernamePasswordLogin extends DispatchSnippet with shiro.SubjectLifeCycle {
   def render = {
     var username = ""
     var password = ""
-    "type=text" #> SHtml.text(username, username = _) &
+    "type=text" #>  FocusOnLoad(SHtml.text(username, username = _)) &
     "type=password" #> SHtml.password(password, password = _) &
     "type=submit" #> SHtml.submit("Login", () => 
       login(new UsernamePasswordToken(username, password)))
